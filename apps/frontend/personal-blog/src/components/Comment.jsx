978105b211c54da425ec68c79a5b81de4e11d18d
dollarwolv <dollarwolv@gmail.com>
2025-12-23 13:41:04 +0800

    F: Add ability to edit comment

--- apps/frontend/personal-blog/src/components/Comment.jsx
+++ apps/frontend/personal-blog/src/components/Comment.jsx
@@ -48,13 +48,14 @@ function Comment({ comment, getComments, postid }) {
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-          text: comment,
+          text: commentText,
         }),
       },
     );
     if (!res.ok) {
       throw new Error("Could not post comment.");
     }
+    setCommentBeingEdited(false);
     getComments();
   }
 
@@ -65,49 +66,75 @@ function Comment({ comment, getComments, postid }) {
   return (
     <div className="flex gap-2 border-t-[0.5px]">
       <img src={userIcon} alt="" className="mt-4 self-start" />
-      <div className="mx-1 my-3 flex flex-col">
-        <div className="flex w-full flex-row items-center gap-2">
+      <div className="mx-1 my-3 mr-auto flex grow flex-col">
+        <div className="mr-auto flex grow flex-row items-center gap-2">
           <span className="font-medium">@{comment.author.username}</span>
           <span className="text-xs font-light">
             {parseDate(comment.createdAt)}
           </span>
         </div>
         {commentBeingEdited ? (
-          <input
-            value={commentText}
-            onChange={(e) => setCommentText(e.target.value)}
-            className="w-full"
-          ></input>
+          <form
+            className="flex flex-col"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleEditComment(comment.id);
+            }}
+          >
+            <input
+              value={commentText}
+              onChange={(e) => setCommentText(e.target.value)}
+              className="w-full rounded border-b-[0.5] border-dotted bg-gray-300/30 p-1"
+            />
+            <div className="mt-1 flex gap-2 self-end">
+              <button
+                type="button"
+                className="rounded border border-dotted px-1.5 py-1 leading-[84%] font-light tracking-tighter whitespace-nowrap uppercase"
+                onClick={() => {
+                  setCommentBeingEdited(false);
+                  setCommentText(comment.text);
+                }}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="rounded bg-black px-1.5 py-1 leading-[84%] tracking-tighter whitespace-nowrap text-white uppercase"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
         ) : (
           <span className="">{comment.text}</span>
         )}
       </div>
-      {(user?.role === "ADMIN" || comment.authorId === user?.id) && (
-        <div className="col-start-17 col-end-18 my-auto ml-auto flex">
-          {comment.authorId === user?.id && (
-            <button
-              onClick={() => {
-                setCommentBeingEdited(true);
-                handleEditComment(comment.id);
-              }}
-              className="w-8"
-            >
-              <img src={edit} alt="edit post" />
-            </button>
-          )}
+      {(user?.role === "ADMIN" || comment.authorId === user?.id) &&
+        !commentBeingEdited && (
+          <div className="col-start-17 col-end-18 my-auto flex">
+            {comment.authorId === user?.id && (
+              <button
+                onClick={() => {
+                  setCommentBeingEdited(true);
+                }}
+                className="w-8"
+              >
+                <img src={edit} alt="edit post" />
+              </button>
+            )}
 
-          <button
-            className="w-8"
-            onClick={(e) => {
-              e.preventDefault();
-              e.stopPropagation();
-              handleDeleteComment(comment.id);
-            }}
-          >
-            <img src={trash} alt="delete post" />
-          </button>
-        </div>
-      )}
+            <button
+              className="w-8"
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                handleDeleteComment(comment.id);
+              }}
+            >
+              <img src={trash} alt="delete post" />
+            </button>
+          </div>
+        )}
     </div>
   );
 }
